aabb3a40e1ae05ec4888ffb6c12512a6d56ef509
Joonas Tapio <joonas.tapio@helsinki.fi>
2024-11-18 15:21:42 +0200

    t2-4 tehty

--- src/app.py
+++ src/app.py
@@ -4,11 +4,19 @@ from counter import Counter
 app = Flask(__name__)
 cnt = Counter()
 
+
 @app.route("/")
 def index():
     return render_template("index.html", value=cnt.value)
 
+
 @app.route("/increment", methods=["POST"])
 def increment():
     cnt.increase()
+    return redirect("/")
+
+
+@app.route("/reset", methods=["POST"])
+def reset():
+    cnt.reset()
     return redirect("/")
